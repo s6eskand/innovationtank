15ac0b765541d216f0a18953fae6ef8115246580
Sam <sam.eskandar00@hotmail.com>
2020-04-12 18:38:20 -0400

    add header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   render() {
     return (
         <div className="App">
             <Navbar/>
+            <Header/>
         </div>
     );
   }
