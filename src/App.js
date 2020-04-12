d63757c9b2b709a53cac2eaf5df72f707f26ef6f
Sam <sam.eskandar00@hotmail.com>
2020-04-12 16:27:34 -0400

    add component dir

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./components/Navbar";
 
 class App extends React.Component {
   render() {
     return (
         <div className="App">
-
+            <Navbar/>
         </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Creating Component Directory</h1>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
